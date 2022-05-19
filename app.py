8e742341152dddc60ad250c609499589bba06673
naqvi29 <63480455+naqvi29@users.noreply.github.com>
2022-05-19 16:27:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'novel-fyp'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_PORT'] = 3307
+app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql.init_app(app)
 
 # configure secret key for session protection)
